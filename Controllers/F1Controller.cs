e4a113dbfb73a7013669bd46d26ab202dbd371d5
Oscar Pinzon <oscar.eugenio.pinzon@gmail.com>
2022-10-02 18:01:40 -0400

    add Driver standings endpoint

---
+++ Controllers/F1Controller.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace BridgitCodingChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class F1Controller : ControllerBase
+    {
+        private readonly ILogger<F1Controller> _logger;
+
+        public F1Controller(ILogger<F1Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{year}/standings")]
+        public IEnumerable<DriverStanding> Get()
+        {
+            Root root = JsonFileReader.Read<Root>("Data/driverStandings.json");
+            return root.DriverStandings;
+        }
+    }
+
+    public static class JsonFileReader
+    {
+        public static T Read<T>(string filePath)
+        {
+            string text = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(text);
+        }
+    }
+}
---
+++ Models/DriverStandings.cs
@@ -0,0 +1,36 @@
+﻿// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Constructor
+    {
+        public string constructorId { get; set; }
+        public string url { get; set; }
+        public string name { get; set; }
+        public string nationality { get; set; }
+    }
+
+    public class Driver
+    {
+        public string driverId { get; set; }
+        public string permanentNumber { get; set; }
+        public string code { get; set; }
+        public string url { get; set; }
+        public string givenName { get; set; }
+        public string familyName { get; set; }
+        public string dateOfBirth { get; set; }
+        public string nationality { get; set; }
+    }
+
+    public class DriverStanding
+    {
+        public string position { get; set; }
+        public string positionText { get; set; }
+        public string points { get; set; }
+        public string wins { get; set; }
+        public Driver Driver { get; set; }
+        public List<Constructor> Constructors { get; set; }
+    }
+
+    public class Root
+    {
+        public List<DriverStanding> DriverStandings { get; set; }
+    }
+
